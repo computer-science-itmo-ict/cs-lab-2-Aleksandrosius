cfd1185c7754a25c62fb74b6c08580a529181222
Александр Холодов-Воронцов <alex-hil2014@yandex.ru>
2021-11-02 17:07:24 +0300

    I did the second task!

---
+++ script.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+x=$1 
+y=$2 
+
+echo $(($x+$y)) $(($x-$y)) $(($x*$y))
+
+if [[ $y -eq 0 ]]
+then	
+    echo "#"
+elif [[ $x%$y -eq 0 ]]
+then
+    echo "$(($x/$y)).00"
+elif [[ $x -eq 0 ]]
+then
+    echo "0.00"
+else
+    sp=$(($x*100/$y))
+    sp1=$(($sp/100))
+    sp2=$(($sp%100))
+    sp3="${sp2#-}"
+    if [[ $x*$y -lt 0 ]] && [[ $x/$y -eq 0 ]]; then
+    	echo "$sp1.$sp3"
+    else echo "-$sp1.$sp3"
+fi
+fi
